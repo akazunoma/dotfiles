13dba6a512c9e60bc50b0d9fbadda27013b66e66
Kazuya Takeshima <mail@mitukiii.jp>
2010-12-28 13:26:55 +0900

    Add init-loader.el. to inie.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,11 @@
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
+;; init loader
+;; (install-elisp "http://coderepos.org/share/export/38744/lang/elisp/init-loader/init-loader.el")
+(require 'init-loader)
+(init-loader-load "~/.emacs.d/init")
+
 ;; language and coding
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
