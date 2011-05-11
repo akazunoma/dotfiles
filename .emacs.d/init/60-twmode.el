48fe05b5673316ba038597b1e1e3ff5f24cb742a
Kazuya Takeshima <mail@mitukiii.jp>
2011-05-11 10:31:52 +0900

    Change status update interval.

--- .emacs.d/init/60-twmode.el
+++ .emacs.d/init/60-twmode.el
@@ -7,7 +7,7 @@
 (setq twittering-icon-mode nil)
 (setq twittering-scroll-mode nil)
 (setq twittering-convert-fix-size 48)
-(setq twittering-timer-interval 60)
+(setq twittering-timer-interval 300)
 (setq twittering-update-status-function 'twittering-update-status-from-pop-up-buffer)
 (add-hook 'twittering-mode-hook
           '(lambda ()
