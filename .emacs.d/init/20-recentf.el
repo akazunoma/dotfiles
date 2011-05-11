05737c285ddb7c781f826f1f020ca61e285e9b86
Kazuya Takeshima <mail@mitukiii.jp>
2011-05-11 10:31:34 +0900

    Add recentf auto cleanup and save timer.

--- .emacs.d/init/20-recentf.el
+++ .emacs.d/init/20-recentf.el
@@ -4,4 +4,7 @@
 (setq recentf-max-menu-items 10000)
 (setq recentf-max-save-items 10000)
 (setq recentf-max-saved-items 10000)
+(setq recentf-auto-cleanup 10)
+(setq recentf-auto-save-timer
+        (run-with-idle-timer 30 t 'recentf-save-list))
 (require 'recentf-ext)
