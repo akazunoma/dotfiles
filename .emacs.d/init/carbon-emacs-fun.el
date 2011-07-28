653863bf17853d7d46f33d4dea23220ad941613a
Kazuya Takeshima <mail@mitukiii.jp>
2011-07-28 16:13:34 +0900

    Add htmlrize to carbon emacs.

--- .emacs.d/init/carbon-emacs-fun.el
+++ .emacs.d/init/carbon-emacs-fun.el
@@ -18,6 +18,21 @@
   (interactive (browse-url-interactive-arg "URL: "))
   (start-process (concat "open " url) nil "open" url "-g"))
 
+(defun htmlize-and-browse ()
+  (interactive)
+  (defcustom
+    htmlize-and-browse-directory-path temporary-file-directory
+    "htmlize-and-browse-temporary-file-directory"
+    :type 'string
+    :group 'htmlize-and-browse)
+  (setq htmlize-and-browse-buffer-file-name (concat "htmlize-and-browse-" (format-time-string "%Y%m%d%H%M%S" (current-time)) ".html"))
+  (setq htmlize-and-browse-buffer-file-path (concat htmlize-and-browse-directory-path htmlize-and-browse-buffer-file-name))
+  (with-current-buffer (htmlize-buffer)
+    (write-file htmlize-and-browse-buffer-file-path)
+    (set-buffer-modified-p nil)
+    (kill-buffer htmlize-and-browse-buffer-file-name)
+    (shell-command (concat "open " htmlize-and-browse-buffer-file-path))))
+
 (define-key global-map (kbd "C-x C-d") 'copy-current-dir)
 (define-key global-map (kbd "C-x C-p") 'copy-current-path)
 (define-key global-map (kbd "C-x C-t") 'open-terminal)
