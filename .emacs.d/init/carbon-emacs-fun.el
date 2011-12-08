1c53c20c453db7cbe002b7e6c919470e17d4bbf4
Kazuya Takeshima <mail@mitukiii.jp>
2011-12-08 16:11:11 +0900

    Add google chrome shoutcut to emacs & screen.

--- .emacs.d/init/carbon-emacs-fun.el
+++ .emacs.d/init/carbon-emacs-fun.el
@@ -10,31 +10,22 @@
   (interactive)
   (shell-command-to-string "open -a terminal"))
 
+(defun open-google-chrome ()
+  (interactive)
+  (shell-command-to-string "open -a google\\ chrome"))
+
 (defun open-yorufukurou ()
   (interactive)
   (shell-command-to-string "open -a yorufukurou"))
+
 
 (defun browse-url-default-macosx-browser (url &optional new-window)
   (interactive (browse-url-interactive-arg "URL: "))
   (start-process (concat "open " url) nil "open" url "-g"))
 
-(defun htmlize-and-browse ()
-  (interactive)
-  (defcustom
-    htmlize-and-browse-directory-path temporary-file-directory
-    "htmlize-and-browse-temporary-file-directory"
-    :type 'string
-    :group 'htmlize-and-browse)
-  (setq htmlize-and-browse-buffer-file-name (concat "htmlize-and-browse-" (format-time-string "%Y%m%d%H%M%S" (current-time)) ".html"))
-  (setq htmlize-and-browse-buffer-file-path (concat htmlize-and-browse-directory-path htmlize-and-browse-buffer-file-name))
-  (with-current-buffer (htmlize-buffer)
-    (write-file htmlize-and-browse-buffer-file-path)
-    (set-buffer-modified-p nil)
-    (kill-buffer htmlize-and-browse-buffer-file-name)
-    (shell-command (concat "open " htmlize-and-browse-buffer-file-path))))
-
 (define-key global-map (kbd "C-x C-d") 'copy-current-dir)
 (define-key global-map (kbd "C-x C-p") 'copy-current-path)
 (define-key global-map (kbd "C-x C-t") 'open-terminal)
+(define-key global-map (kbd "C-x C-g") 'open-google-chrome)
 (define-key global-map (kbd "A-E") 'open-yorufukurou)
 
