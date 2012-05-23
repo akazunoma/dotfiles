0f088ab2749588da086b62c58338f2da9e7ae4d3
Kazuya Takeshima <mail@mitukiii.jp>
2012-05-23 11:03:02 +0900

    .screenrc / .zsh のメジャーモード設定

--- .emacs.d/init/40-others.el
+++ .emacs.d/init/40-others.el
@@ -3,6 +3,12 @@
 ;; generic-x mode
 (require 'generic-x)
 
+;; shell script mode
+(add-to-list 'auto-mode-alist '("\\.zsh" . shell-script-mode))
+
+;; conf mode
+(add-to-list 'auto-mode-alist '("\\.screenrc" . conf-space-mode))
+
 ;; yaml mode
 ;; (install-elisp "http://github.com/yoshiki/yaml-mode/raw/master/yaml-mode.el")
 (autoload 'yaml-mode "yaml-mode" nil t)
