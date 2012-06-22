ed7a3aacbb6f478a381646528685291908b02a1b
Kazuya Takeshima <mail@mitukiii.jp>
2012-06-22 12:51:17 +0900

    recentf の保存数めっちょ増やした

--- .emacs.d/init/20-extension.el
+++ .emacs.d/init/20-extension.el
@@ -13,9 +13,9 @@
 
 ;; recentf-ext
 ;; (install-elisp "http://www.emacswiki.org/cgi-bin/wiki/download/recentf-ext.el")
-(setq recentf-max-menu-items 10000)
-(setq recentf-max-save-items 10000)
-(setq recentf-max-saved-items 10000)
+(setq recentf-max-menu-items 100000)
+(setq recentf-max-save-items 100000)
+(setq recentf-max-saved-items 100000)
 (setq recentf-auto-cleanup 'never)
 (setq recentf-auto-save-timer
         (run-with-idle-timer 30 t 'recentf-save-list))
