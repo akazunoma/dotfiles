4ac853e0b339e46e8a8fea5ecca3ccb0649b9a68
Kazuya Takeshima <mail@mitukiii.jp>
2012-11-17 20:22:30 +0900

    Add nginx mode.

--- .emacs.d/init/40-others.el
+++ .emacs.d/init/40-others.el
@@ -9,6 +9,11 @@
 ;; conf mode
 (add-to-list 'auto-mode-alist '("\\.screenrc" . conf-space-mode))
 
+;; nginx mode
+;; (install-elisp "https://raw.github.com/ajc/nginx-mode/master/nginx-mode.el")
+(autoload 'nginx-mode "nginx-mode" nil t)
+(add-to-list 'auto-mode-alist '("/etc/nginx/" . nginx-mode))
+
 ;; yaml mode
 ;; (install-elisp "http://github.com/yoshiki/yaml-mode/raw/master/yaml-mode.el")
 (autoload 'yaml-mode "yaml-mode" nil t)
