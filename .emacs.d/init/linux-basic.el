8c7a8aa08d005f66b31d8498cc61eb3e8e7c7fdc
Kazuya Takeshima <mail@mitukiii.jp>
2012-12-01 02:03:42 +0900

    migemo のパスの設定を OS ごとに分割

---
+++ .emacs.d/init/linux-basic.el
@@ -0,0 +1,6 @@
+;; Linux
+
+;; migemo
+;; config with "extension.el"
+(setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
+(migemo-init)
---
+++ .emacs.d/init/mac-basic.el
@@ -0,0 +1,6 @@
+;; Mac
+
+;; migemo
+;; config with "extension.el"
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(migemo-init)
