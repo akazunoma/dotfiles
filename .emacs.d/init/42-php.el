591e2b4cad142726ed708e7707e2b5b1c9b11ab0
Kazuya Takeshima <mail@mitukiii.jp>
2012-11-30 15:32:40 +0900

    最新の php-mode.el 追加

--- .emacs.d/init/42-php.el
+++ .emacs.d/init/42-php.el
@@ -1,7 +1,9 @@
 ;; PHP
 
 ;; php mode
-(require 'php-mode)
+;; (install-elisp "http://php-mode.svn.sourceforge.net/svnroot/php-mode/tags/php-mode-1.5.0/php-mode.el")
+(autoload 'php-mode "php-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-hook 'php-mode-hook
           '(lambda()
              (define-key php-mode-map (kbd "C-\\") 'html-mode)))
