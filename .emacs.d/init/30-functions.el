c0ef2c56b719e35d3d9741007ab566c45b67bb8c
Kazuya Takeshima <mail@mitukiii.jp>
2012-11-30 17:24:44 +0900

    Window を逆に進む関数追加

--- .emacs.d/init/30-functions.el
+++ .emacs.d/init/30-functions.el
@@ -15,17 +15,23 @@
            (point))))
     (skip-chars-forward "\s " point-of-indentation)))
 
-(defun other-window-or-split-vertically ()
+(defun next-window-or-split-vertically ()
   (interactive)
   (when (one-window-p)
     (split-window-vertically))
   (other-window 1))
 
-(defun other-window-or-split-horizontally ()
+(defun next-window-or-split-horizontally ()
   (interactive)
   (when (one-window-p)
     (split-window-horizontally))
   (other-window 1))
+
+(defun prev-window-or-split-horizontally ()
+  (interactive)
+  (when (one-window-p)
+    (split-window-horizontally))
+  (other-window -1))
 
 (setq growl-program (executable-find "growlnotify"))
 (defun growl (title message &optional app)
