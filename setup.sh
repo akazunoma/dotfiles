25ea95c890b4d0070dc38960fb16983421534e0c
Kazuya Takeshima <mail@mitukiii.jp>
2012-12-11 15:48:58 +0900

    setup.sh 書き直した

--- setup.sh
+++ setup.sh
@@ -1,39 +1,40 @@
 #!/bin/sh
 
-DOTFILES_DIR=$(cd $(dirname $0) && pwd)
-
 ## functions
-function _execute() {
+function execute() {
     echo $*
     eval $*
 }
 
-function _create_symlink_unless_exists() {
+function create_symlink_unless_exists() {
     if [ -e $2 ]; then
         echo "$2: File exists"
     else
-        _execute "ln -s $1 $2"
+        execute "ln -s $1 $2"
     fi
 }
+
+## dotfiles dir
+DOTFILES_DIR=$(cd $(dirname $0) && pwd)
 
 ## dotfiles
 DOTFILES=(.zshrc .zsh .emacs.d .gitconfig .gitignore .screenrc .gemrc .pryrc .percol.d)
 for name in ${DOTFILES[@]}
 do
-    _create_symlink_unless_exists $DOTFILES_DIR/$name $HOME/$name
+    create_symlink_unless_exists $DOTFILES_DIR/$name $HOME/$name
 done
 
 ## .screenrc.local
 if [[ $OSTYPE =~ "darwin" ]]; then
-    _create_symlink_unless_exists $DOTFILES_DIR/.screenrc.osx $HOME/.screenrc.local
+    create_symlink_unless_exists $DOTFILES_DIR/.screenrc.osx $HOME/.screenrc.local
 else
-    _create_symlink_unless_exists $DOTFILES_DIR/.screenrc.linux $HOME/.screenrc.local
+    create_symlink_unless_exists $DOTFILES_DIR/.screenrc.linux $HOME/.screenrc.local
 fi
 
 ## .zshrc.local
-_execute "touch $HOME/.zshrc.local"
+execute "touch $HOME/.zshrc.local"
 
 ## .rsense
 if which ruby > /dev/null; then
-    _execute "ruby $DOTFILES_DIR/.emacs.d/elisp/rsense/etc/config.rb > ~/.rsense"
+    execute "ruby $DOTFILES_DIR/.emacs.d/elisp/rsense/etc/config.rb > ~/.rsense"
 fi
